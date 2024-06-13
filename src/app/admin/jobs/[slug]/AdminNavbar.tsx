61a327edb1aeede6b7e08637f07a949edfc83ffa
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-13 11:05:12 +0530

    Approve and Delete Job Functionality added along with Pagination

---
+++ src/app/admin/jobs/[slug]/AdminNavbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useClerk } from "@clerk/nextjs";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function AdminNavbar() {
+  const { user, signOut } = useClerk();
+  const router = useRouter();
+
+  return (
+    <div className="px-3">
+      <div className="m-auto flex h-10 max-w-5xl items-center justify-between gap-2">
+        <Link href="/admin" className="font-semibold underline">
+          Admin Dashboard
+        </Link>
+        <button
+          className="underline"
+          onClick={async () => {
+            await signOut();
+            router.push("/");
+          }}
+        >
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+}
